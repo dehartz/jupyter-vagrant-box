ab50a1491ec23e8e76944cbf7d5b8b226556edc3
dehartz <zach@example.com>
2019-09-29 14:26:27 -0400

    Prettify and fix rm error
    Prettify syntax and take out removes that were in error.

--- scripts/provision-ijava.sh
+++ scripts/provision-ijava.sh
@@ -2,23 +2,18 @@
 
 cd /home/vagrant/data
 
-sudo apt-get install unzip
-
+echo "Istalling openjdk..."
 wget -q https://download.java.net/openjdk/jdk9/ri/openjdk-9+181_linux-x64_ri.zip
-
 sudo mkdir /usr/lib/jvm
-
 sudo unzip openjdk-9+181_linux-x64_ri.zip -d /usr/lib/jvm
-
 rm -f openjdk-9+181_linux-x64_ri.zip
+echo "Done!"
 
+echo "Installing IJava..."
 wget -q https://github.com/SpencerPark/IJava/releases/download/v1.3.0/ijava-1.3.0.zip
-
-unzip -o ijava-1.3.0.zip
-
+unzip -o ijava-1.3.0.zip -d /home/vagrant
 rm -f ijava-1.3.0.zip
 
-python install.py --sys-prefix
 
-rm -rf './java/'
-rm -f './install.py'
+python /home/vagrant/install.py --sys-prefix
+echo "Done!"
